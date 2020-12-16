6323572ae5ed09159f386931462c0134ad91e244
Suyashkala18 <76032562+Suyashkala18@users.noreply.github.com>
2020-12-16 10:42:04 +0530

    Create class2.cs
    changes good to go for 2nd programmer

---
+++ SampleApplicationDay2Solution/SampleApplicationDay2/class2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplicationDay2
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("changes done by 2nd programmer");
+        }
+    }
+}
